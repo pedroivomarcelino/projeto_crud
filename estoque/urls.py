35f826b36cfe12a9dc87e2b379138b36e6c01d15
pedro.ivo <pedroivo.marcelino@hotmail.com>
2025-04-14 19:38:45 -0300

    implementado metodo update de produtos

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from estoque.views import cadastrarProdutos, inserirProduto, vizualizarProdutos
+from estoque.views import cadastrarProdutos, inserirProduto, vizualizarProdutos, editarProdutos, updateProdutos
 
 urlpatterns = [
     path('cadastrar-produtos', cadastrarProdutos, name='cadastrar-produtos'),
     path('inserir-produto', inserirProduto, name='inserir-produto'),
     path('visualizar-produtos/<int:id>', vizualizarProdutos, name='visualizar-produtos'),
+    path('editar-produtos/<int:id>', editarProdutos, name='editar-produtos'),
+    path('upadate-produtos/<int:id>', updateProdutos, name='update-produtos'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from estoque.forms import ProdutoForm
 from django.contrib import messages
 from estoque.models import Produto
@@ -13,6 +13,12 @@ def cadastrarProdutos(request):
 def vizualizarProdutos(request, id):
    produtos = Produto.objects.get(id=id)
    return render(request, 'estoque/visualisar_produto.html', {'produtos': produtos})
+
+
+#carrega o form de edicao de produtos
+def editarProdutos(request, id):
+    produtos = Produto.objects.get(id=id)
+    return render(request, 'estoque/editar_produto.html', {'produtos': produtos})
 
 
 #funcao para inserir novos produtos no banco de dados
@@ -34,3 +40,28 @@ def inserirProduto(request):
                     messages.error(request, f"{form.fields[field].label}: {error}", extra_tags='danger')
                     
     return redirect('cadastrar-produtos')
+
+
+#funcao para atualizar os produtos cadastrados no banco de dados
+def updateProdutos(request, id):
+    
+    produtos = get_object_or_404(Produto, id=id)
+    
+    if request.method == 'POST':
+     
+        form = ProdutoForm(request.POST, instance=produtos)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Produto atualizado com sucesso!")
+        else:
+            messages.error(request, "Erro ao atualizar o produto. Verifique os dados e tente novamente.")
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{form.fields[field].label}: {error}", extra_tags='danger')
+                    return redirect('editar-produtos', id=id)
+    
+    else:
+        form = ProdutoForm(instance=produtos)
+        
+    return redirect('editar-produtos', id=id)
+                    
