45c21ebb28f7e0862835e21acd1e06308a967e34
pedro.ivo <pedroivo.marcelino@hotmail.com>
2025-05-08 19:35:58 -0300

    alterado layout do menu latetal separando por blocos

--- setup/settings.py
+++ setup/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'usuario.context_processors.usuario_logado',
+                'django.template.context_processors.request',
                 
             ],
         },
